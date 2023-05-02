876fe9f7d9578c3f41874c6cd1c8fc629d0e04f2
Armans0701~ <sikder89098@gmail.com>
2023-05-03 01:06:17 +0600

    chefs data loaded

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 const chefs = require ('./data/chef.json')
-
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Chef is running");
 });
 
 app.get('/chefs',(req,res)=>{
+    console.log(chefs)
 res.send(chefs)
 })
 
